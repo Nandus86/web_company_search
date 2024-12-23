70b9c022baf445116b73967f1cb5e15d1ef27262
Nandus86 <nandusempreendimentos@gmail.com>
2024-12-23 11:14:20 -0300

    1.0.3

--- __manifest__.py
+++ __manifest__.py
@@ -11,7 +11,7 @@
         - Integração com res.partner
     ''',
     'category': 'Tools',
-    'author': 'Fernando Dias - v.1.0.2',
+    'author': 'Fernando Dias - v.1.0.3',
     'license': 'LGPL-3',
     'depends': ['base', 'web', 'contacts'],
     'data': [
